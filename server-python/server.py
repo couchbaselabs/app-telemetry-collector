305abcfdecc6469499ef68565c0639b2122c583c
Sergey Avseyev <sergey.avseyev@gmail.com>
2025-01-21 10:17:09 -0800

    add echo endpoint for testing purposes

--- server-python/server.py
+++ server-python/server.py
@@ -63,11 +63,43 @@ async def poll_app_telemetry(ws, store: TelemetryStore):
         await ws.close()
         print("WebSocket closed.")
 
+async def poll_echo(ws):
+    try:
+        async for msg in ws:
+            print(f"INCOMING: {msg.type}")
+            if msg.type == web.WSMsgType.TEXT:
+                print(f"TEXT: {msg.data}")
+                await ws.send_str(msg.data)
+            elif msg.type == web.WSMsgType.BINARY:
+                print(f"BINARY: {msg.data.decode('utf-8')}")
+                await ws.send_bytes(msg.data)
+            elif msg.type == web.WSMsgType.CLOSE:
+                print("CLOSE")
+                break
+    except asyncio.TimeoutError:
+        print("Timeout: No message received within the specified time.")
+        await ws.close()
+    except asyncio.CancelledError:
+        print("Polling task cancelled.")
+    except Exception as e:
+        print(f"Error in websocket: {e}")
+    finally:
+        await ws.close()
+        print("WebSocket closed.")
+
+
 # WebSocket handler for connection upgrades
 async def websocket_handler(request, store: TelemetryStore):
     ws = web.WebSocketResponse()
     await ws.prepare(request)
     await poll_app_telemetry(ws, store)
+    return ws
+
+async def echo_handler(request):
+    print("start echo_handler")
+    ws = web.WebSocketResponse(heartbeat = 3.0)
+    await ws.prepare(request)
+    await poll_echo(ws)
     return ws
 
 # REST API to get telemetry metrics
@@ -83,6 +115,7 @@ async def init_app():
     app = web.Application()
     app.router.add_get('/metrics', lambda request: metrics(request, store))
     app.router.add_get('/app_telemetry', lambda request: websocket_handler(request, store))
+    app.router.add_get('/echo', lambda request: echo_handler(request))
     return app
 
 # Run the application
